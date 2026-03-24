bf6e44c3791c9b6b6645d4057873c69086a0181d
Trotska <lagiu4@gmail.com>
2026-03-24 17:16:24 +1000

    Assignment DB Model Created
    Created a db model for assignments
    
    this features name, userId, description, course and date.

---
+++ backend/models/Assignment.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+// MongoDB model for assignments
+//Potential Ideas: name, date, priority, description, course, status (e.g., pending, completed)
+const assignmentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    description: { type: String },
+    course: { type: String },
+    date: { type: Date, required: true, unique: true },
+    priority: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Assignment", assignmentSchema);
