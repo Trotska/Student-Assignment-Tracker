f8f9978d1d81341e2e21c046ddab57eada22a065
Trotska <lagiu4@gmail.com>
2026-04-03 23:42:06 +1000

    Assignment Delete routes added
    Assignments weren't deleting due to bad routing.

--- backend/routes/assignmentRoutes.js
+++ backend/routes/assignmentRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createAssignment, getAssignmentsByUser, updateAssignment } = require('../controllers/assignmentController');
+const { createAssignment, getAssignmentsByUser, updateAssignment, deleteAssignment } = require('../controllers/assignmentController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', protect, createAssignment);
 router.get('/', protect, getAssignmentsByUser);
 router.put('/:id', protect, updateAssignment);
+router.delete('/:id', protect, deleteAssignment);
 
 module.exports = router;
