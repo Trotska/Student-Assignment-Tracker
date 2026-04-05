52597940e6fb222efb2ee0d8770ab26293bc04d7
Trotska <lagiu4@gmail.com>
2026-04-05 15:51:55 +1000

    changed over ip due to ec2 shutdown

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.236.95.19:5001', // live
+  baseURL: 'http://16.176.67.173:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
